aed24c192fc1f676cfd879fbc9130c616b3cc52b
desmondhama <desmondhama@gmail.com>
2025-03-09 20:01:38 +0200

    Update MainActivity.kt

--- app/src/main/java/com/nexera/basicinfocardsapp/MainActivity.kt
+++ app/src/main/java/com/nexera/basicinfocardsapp/MainActivity.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,6 +22,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
 import com.nexera.basicinfocardsapp.ui.theme.MyComposeApplicationTheme
 
@@ -53,23 +56,33 @@ fun FourEqualParts() {
                 .fillMaxWidth(0.5f)
                 .fillMaxHeight(0.5f)
                 .background(color = Color(0xFFEADDFF))
-                .align(Alignment.TopStart)
+                .align(Alignment.TopStart),
+				contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = stringResource(R.string.title_text),
-                style = TextStyle(
+            Column(
+			    modifier = Modifier.padding(16.dp)
+				) {
+			    Text(
+                    text = stringResource(R.string.title_text),
+                    style = TextStyle(
                     fontWeight = FontWeight.Bold
-                ),
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
+                    ),
+					modifier = Modifier
+					.fillMaxWidth()
+					.padding(bottom = 16.dp),
+				    textAlign = TextAlign.Center
+                )
+			    Text(
+                    text = stringResource(R.string.infomation_text),
+                    style = TextStyle(
+                                fontSize = 18.sp
+                            ),
+					modifier = Modifier
+					.fillMaxWidth()
+					.padding(bottom = 16.dp),
+				    textAlign = TextAlign.Center
+                )
+			}
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyComposeApplicationTheme {
-        FourEqualParts()
     }
 }
